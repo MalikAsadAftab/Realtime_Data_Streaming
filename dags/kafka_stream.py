dff28d8ab433f9aa5cce46ff62069f4e389d73d4
Asad Aftab <asadaftabmalik@gmail.com>
2024-07-18 13:13:02 +0200

    Update kafka_stream.py

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -4,8 +4,8 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 default_args = {
-    'owner': 'airscholar',
-    'start_date': datetime(2023, 9, 3, 10, 00)
+    'owner': 'AsadAftab',
+    'start_date': datetime(2024, 7, 10, 12, 00)
 }
 
 def get_data():
